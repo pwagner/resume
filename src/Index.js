902d17b06726686dec70b031114c9b2e96ee631d
Paul Wagner <npm.pwagner@icloud.com>
2016-09-12 15:01:58 +0200

    added promise polyfill for IE

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { polyfill } from 'es6-promise';
 import App from './containers/App';
 
+polyfill();
 ReactDOM.render(
   <App source="/dist/resume.json" />, document.getElementById('content')
 );
